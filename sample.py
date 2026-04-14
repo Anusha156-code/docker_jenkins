fa7510f7746cceb6ebbfec369c9c9c81d5ab59f9
Anusha <anusha4615@gmail.com>
2026-04-14 16:26:22 +0530

    checking poll scm in jenkins!

--- sample.py
+++ sample.py
@@ -3,7 +3,7 @@ app = Flask (__name__)
 
 @app.route("/")
 def container():
-    return "Practicing the tools in devops"
+    return "Practicing the tools in devops.."
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 5000)
 
